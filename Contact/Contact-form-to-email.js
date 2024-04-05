4838d9003f8d5d4e87d1d44ce2e01904e1b9d972
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2024-04-05 20:50:16 +0530

    Update Contact-form-to-email.js

--- Contact/Contact-form-to-email.js
+++ Contact/Contact-form-to-email.js
@@ -1,9 +1,13 @@
 function emailSend() {
 	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
 	var email = document.getElementById('email').value;
+	var message = document.getElementById('message').value;
   
 	var messageBody = "Name: " + userName +
-	  "<br/> Email: " + email;
+	  "<br/> Phone: " + phone +
+	  "<br/> Email: " + email +
+	  "<br/> Message: " + message;
   
 	Email.send({
 		SecureToken: "f1fd93e4-78df-4474-8db5-80daf880c520",
